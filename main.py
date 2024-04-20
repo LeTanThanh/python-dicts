cf96f9d2134f20c9863e6b1c41552901da1d45a7
TanThanhLe <tanthanhcr95@gmail.com>
2024-04-21 01:35:35 +0700

    Defining a Dictionary

--- main.py
+++ main.py
@@ -16,3 +16,68 @@ if __name__ == "__main__":
   - List elements are accessed by their position in the list, via indexing.
   - Dictionary elements are accessed via keys.
   """
+
+  # Defining a Dictionary
+
+  """
+  Dictionaries are Python's implementation of a data structure that is more generally known as an associative array.
+  A dictionary consist of a collection of key-value pairs.
+  Each key-value pair maps the key to its associated value.
+
+  You can define a dictionary by enclosing a comma-separated list of key-value pairs in curly braces ({}).
+  A colon (:) separates each key from its associated value:
+
+  The following defined a dictionary that mnaps a location to the name of its corresponding Major League Baseball team:
+  """
+
+  MLB_team = {
+    "Colorado": "Rockies",
+    "Boston": "Red Sox",
+    "Minnesota": "Twins",
+    "Milwaukee": "Brewers",
+    "Seattle": "Mariners"
+  }
+
+  """
+  You can also construct a dictionary with the built-in dict() function.
+  The argument to dict() function should be a sequence of key-value pairs.
+  A list of tuples works well for this
+  """
+
+  MLB_team = dict([
+    ("Colorado", "Rockies"),
+    ("Boston", "Red Sox"),
+    ("Minnesota", "Twins"),
+    ("Milwaukee", "Brewers"),
+    ("Seattle", "Mariners")
+  ])
+
+  """
+  If the key values are simple strings, they can be specificed as keyword arguments.
+  So here is yet another way to define MLB_team:
+  """
+
+  MLB_team = dict(
+    Colorado = "Rockies",
+    Boston = "Red Sox",
+    Minnesota = "Twins",
+    Milwaukee = "Brewers",
+    Seattle = "Mariners"
+  )
+
+  """
+  Once you've defined a dictionary, you can display its contents, the sane as you can do for a list.
+  All three of the definitions shown above appear as follows when displayed:
+  """
+
+  print(MLB_team)
+  print(type(MLB_team))
+
+  """
+  The entries in the dictionary display in the order they were defined.
+  But that is irrelevant when it comes to retrieving them.
+  Dictionary elements are not accessed by numerical index:
+
+  Perhaps you'd still like to sort your dictionary.
+  If that's the case, then checkout Soring a Python Dictionary: Values, Keys and More.
+  """
